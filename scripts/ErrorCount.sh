07e6bc630b7ca5cfb96675e6488239888c534711
jpuritz <jpuritz@tamu.edu>
2015-03-18 12:41:24 -0500

    Update to ErrorCount
    Simplifies error counting code and adds compatibility with phased VCF files.

--- scripts/ErrorCount.sh
+++ scripts/ErrorCount.sh
@@ -3,53 +3,41 @@
 echo "This script counts the number of potential genotyping errors due to low read depth"
 echo "It report a low range, based on a 50% binomial probability of observing the second allele in a heterozygote and a high range based on a 25% probability."
 
-R1H01S0=$(grep -wo 0/1:1:0 $1 | wc -l) 
-R1H01S1=$(grep -wo 0/1:1:1 $1 | wc -l) 
-R1H0S1=$(grep -wo 0/0:1:0 $1 | wc -l) 
-R1H0S0=$(grep -wo 0/0:1:1 $1 | wc -l) 
-R1H1S1=$(grep -wo 1/1:1:0 $1 | wc -l) 
-R1H1S0=$(grep -wo 1/1:1:1 $1 | wc -l) 
+R1H01S0=$(grep -oh '0[/|]1:1:' $1 | wc -l) 
+R1H01S1=$(grep -oh '1[/|]0:1:' $1 | wc -l) 
+R1H0S1=$(grep -oh '0[/|]0:1:' $1 | wc -l) 
+R1H1S1=$(grep -oh '1[/|]1:1:' $1 | wc -l) 
 
-R1GEN=$(python -c "print $R1H01S0+$R1H01S1+$R1H0S0+$R1H0S1+$R1H1S0+$R1H1S1")
+R1GEN=$(python -c "print $R1H01S0+$R1H01S1+$R1H0S1+$R1H1S1")
 R1ERR1=$(python -c "print $R1GEN/2")
 R1ERR2=$(python -c "print $R1GEN*0.75")
 
 echo "Potential genotyping errors from genotypes from only 1 read range from $R1ERR1 to $R1ERR2" 
 
-R2H01S0=$(grep -wo 0/1:2:0 $1 | wc -l) 
-R2H01S2=$(grep -wo 0/1:2:2 $1 | wc -l) 
-R2H0S1=$(grep -wo 0/0:2:0 $1 | wc -l) 
-R2H0S01=$(grep -wo 0/0:2:1 $1 | wc -l)
-R2H0S0=$(grep -wo 0/0:2:2 $1 | wc -l)
-R2H1S01=$(grep -wo 1/1:2:2 $1 | wc -l) 
-R2H1S0=$(grep -wo 1/1:2:1 $1 | wc -l) 
-R2H1S1=$(grep -wo 1/1:2:0 $1 | wc -l)
+R2H01S0=$(grep -oh '0[/|]1:2:[02]' $1 | wc -l) 
+R2H01S2=$(grep -oh '1[/|]0:2:[02]' $1 | wc -l) 
+R2H0S1=$(grep -oh '0[/|]0:2:' $1 | wc -l) 
+R2H1S1=$(grep -oh '1[/|]1:2:' $1 | wc -l)
 
-R2GEN=$(python -c "print $R2H01S0+$R2H01S1+$R2H0S0+$R2H0S1+$R2H0S01+$R2H1S0+$R2H1S1+$R2H1S01")
+R2GEN=$(python -c "print $R2H01S0+$R2H01S2+$R2H1S1+$R2H0S1")
 R2ERR1=$(python -c "print $R2GEN/4")
 R2ERR2=$(python -c "print $R2GEN* 0.5625")
 
 echo "Potential genotyping errors from genotypes from only 2 reads range from $R2ERR1 to $R2ERR2" 
 
-R3H01S0=$(grep -wo 0/1:3:0 $1 | wc -l) 
-R3H01S2=$(grep -wo 0/1:3:3 $1 | wc -l) 
-R3H0S1=$(grep -wo 0/0:3:0 $1 | wc -l) 
-R3H0S01=$(grep -wo 0/0:3:1 $1 | wc -l)
-R3H0S0=$(grep -wo 0/0:3:2 $1 | wc -l)
-R3H0S3=$(grep -wo 0/0:3:3 $1 | wc -l)
-R3H1S01=$(grep -wo 1/1:3:2 $1 | wc -l) 
-R3H1S0=$(grep -wo 1/1:3:1 $1 | wc -l) 
-R3H1S1=$(grep -wo 1/1:3:0 $1 | wc -l)
-R3H1S3=$(grep -wo 1/1:3:3 $1 | wc -l)
+R3H01S0=$(grep -oh '0[/|]1:3:[03]' $1 | wc -l) 
+R3H01S2=$(grep -oh '1[/|]0:3:[03]' $1 | wc -l) 
+R3H0S1=$(grep -oh '0[/|]0:3:' $1 | wc -l)
+R3H1S3=$(grep -oh '1[/|]1:3:' $1 | wc -l)
 
-R3GEN=$(python -c "print $R3H01S0+$R3H01S1+$R3H0S0+$R3H0S1+$R3H0S01+$R3H1S0+$R3H1S1+$R3H1S01+$R3H0S3+$R3H1S3")
+R3GEN=$(python -c "print $R3H01S0+$R3H01S2+$R3H0S1+$R3H1S3")
 R3ERR1=$(python -c "print $R3GEN/8")
 R3ERR2=$(python -c "print $R3GEN*0.42")
 
 echo "Potential genotyping errors from genotypes from only 3 reads range from $R3ERR1 to $R3ERR2"
 
-R4H0=$(grep -oh '0/0:4:' $1 | wc -l)
-R4H1=$(grep -oh '1/1:4:' $1 | wc -l)
+R4H0=$(grep -oh '0[/|]0:4:' $1 | wc -l)
+R4H1=$(grep -oh '1[/|]1:4:' $1 | wc -l)
 
 R4GEN=$(python -c "print $R4H0+$R4H1")
 R4ERR1=$(python -c "print $R4GEN/16")
@@ -57,8 +45,8 @@ R4ERR2=$(python -c "print $R4GEN*0.316")
 
 echo "Potential genotyping errors from genotypes from only 4 reads range from $R4ERR1 to $R4ERR2"
 
-R5H0=$(grep -oh '0/0:5:' $1 | wc -l)
-R5H1=$(grep -oh '1/1:5:' $1 | wc -l)
+R5H0=$(grep -oh '0[/|]0:5:' $1 | wc -l)
+R5H1=$(grep -oh '1[/|]1:5:' $1 | wc -l)
 
 R5GEN=$(python -c "print $R5H0+$R5H1")
 R5ERR1=$(python -c "print $R5GEN/32")
