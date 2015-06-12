9ef07fe1109b4993bd3b68e468a896d455903fd8
jpuritz <jpuritz@tamu.edu>
2015-06-12 16:11:08 -0500

    Update remove.bad.hap.loci.sh

--- scripts/remove.bad.hap.loci.sh
+++ scripts/remove.bad.hap.loci.sh
@@ -10,7 +10,7 @@ mawk -v x=${NAMES[0]} '$0 !~ x' $NAME.recode.vcf > $NAME.0.t.vcf
 for ((i = 1; i < $LEN; i++));
 do
 j=$(($i - 1))
-mawk -v x=${NAMES[$i]} '!/x/' $NAME.$j.t.vcf > $NAME.$i.t.vcf
+mawk -v x=${NAMES[$i]} '$0 !~ x' $NAME.$j.t.vcf > $NAME.$i.t.vcf
 done
 
 LAST=$(($LEN - 1))
