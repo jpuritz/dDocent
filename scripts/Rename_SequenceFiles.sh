df26a6d053c7c914979a9b0eb369e75133fb1d79
jpuritz <jpuritz@tamu.edu>
2015-06-22 12:41:00 -0500

    Update Rename_SequenceFiles.sh

--- scripts/Rename_SequenceFiles.sh
+++ scripts/Rename_SequenceFiles.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+#This script can quickly rename filess within the dDocent naming convention. It needs to be passed a tab delimited
+#file with the old name in one column and the new name in the other
+#Example#
+#PopA_001  NewPop_001
+#
+# This will renmae PopA_001.F.fq.gz and PopA_001.R.fq.gz to NewPop_001.F.fq.gz and NewPop_001.R.fq.gz
+
 if [ -z "$1" ]
 then
-echo "No file with barcodes and sample names specified."
-echo "Correct usage: Rename_for_dDocent.sh barcodefile"
+echo "No file with old names and new names specified."
+echo "Correct usage: Rename_for_dDocent.sh namesfile"
 exit 1
 else
 NAMES=( `cut -f1  $1 `)
