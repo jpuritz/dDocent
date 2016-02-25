a720563097158b5799f26bf12e5733d134914d96
jpuritz <jpuritz@tamu.edu>
2016-02-25 15:54:07 -0500

    File to rename demultiplexed files with INDICES
    This is useful if you used process_radtags to demultiplex files with inline barcodes and an Illumina index

---
+++ Rename_for_dDocent_with-INDEX.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+echo "No file with barcodes and sample names specified."
+echo "Correct usage: Rename_for_dDocent.sh barcodefile"
+exit 1
+else
+NAMES=( `cut -f1  $1 `)
+BARCODES=( `cut -f2 $1 `)
+BARCODES2=( `cut -f3 $1 `)
+LEN=( `wc -l $1 `)
+LEN=$(($LEN - 1))
+
+echo ${NAMES[0]}
+echo ${BARCODES[0]}
+
+for ((i = 0; i <= $LEN; i++));
+do
+mv sample_${BARCODES[$i]}-${BARCODES2[$i]}.1.fq.gz ${NAMES[$i]}.F.fq.gz
+mv sample_${BARCODES[$i]}-${BARCODES2[$i]}.2.fq.gz ${NAMES[$i]}.R.fq.gz
+done
+fi
