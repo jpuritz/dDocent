e282971077d61dbfe5ef33463306ba68cd7abf8f
TomaszSuchan <tomasz.suchan@gmail.com>
2016-12-01 11:39:21 +0100

    Updated dDocent to work with current vcftools while keeping backwards compatibility

--- scripts/filter_missing_ind.sh
+++ scripts/filter_missing_ind.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-vcftools --vcf $1 --missing --out $2
+#check for vcftools version
+VCFTV=$(vcftools | grep VCF | grep -oh '[0-9]*[a-z]*)$' | sed 's/[a-z)]//')
+        if [ "$VCFTV" -lt "10" ]; then
+                echo "The version of VCFtools installed in your" '$PATH' "is not optimized for dDocent."
+                echo "Please install at least version 0.1.11"
+                exit 1
+        elif [ "$VCFTV" -lt "13" ]; then
+                VCFMISSINGFLAG="--missing"
+        elif [ "$VCFTV" -ge "13" ]; then
+                VCFMISSINGFLAG="--missing-indv"
+        fi
+
+vcftools --vcf $1 $VCFMISSINGFLAG --out $2
 
 CUTOFF=$(mawk '!/IN/' $2.imiss | cut -f5 | sort -rn | perl -e '$d=.14;@l=<>;print $l[int($d*$#l)]')
 #echo $CUTOFF
--- scripts/pop_missing_filter.sh
+++ scripts/pop_missing_filter.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+#check for vcftools version
+VCFTV=$(vcftools | grep VCF | grep -oh '[0-9]*[a-z]*)$' | sed 's/[a-z)]//')
+        if [ "$VCFTV" -lt "10" ]; then
+                echo "The version of VCFtools installed in your" '$PATH' "is not optimized for dDocent."
+                echo "Please install at least version 0.1.11"
+                exit 1
+        elif [ "$VCFTV" -lt "13" ]; then
+                VCFMISSINGFLAG="--missing"
+        elif [ "$VCFTV" -ge "13" ]; then
+                VCFMISSINGFLAG="--missing-indv"
+        fi
+
 if [[ -z "$2" ]]; then
 echo "Usage is pop_missing_filter vcffile popmap percent_missing_per_pop number_of_pops_for_cutoff name_for_output"
 exit 1
@@ -11,7 +23,7 @@ rm badloci
 for i in "${POPS[@]}"
 do
 grep -w $i $2 | cut -f1 > keep.$i
-vcftools --vcf $1 --keep keep.$i --missing --out $i 
+vcftools --vcf $1 --keep keep.$i $VCFMISSINGFLAG --out $i 
 mawk '!/CHROM/' $i.lmiss | mawk -v x=$3 '$6 > x' | cut -f1,2 >> badloci
 done
 
