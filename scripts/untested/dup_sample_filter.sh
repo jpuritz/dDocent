94018fa83e3d50f52072eb388efb46d4783b1c19
Jon Puritz <jpuritz@gmail.com>
2020-07-20 17:31:00 -0400

    Update dup_sample_filter.sh
    Fixes issue with repeat sample names

--- scripts/untested/dup_sample_filter.sh
+++ scripts/untested/dup_sample_filter.sh
@@ -27,20 +27,25 @@ echo "${NAM[$i]}" > keep.${NAM[$i]}
 vcftools --vcf $1 --keep keep.${NAMES[$i]} --recode --recode-INFO-all --minDP 5 --out ${NAMES[$i]}
 vcftools --vcf $1 --keep keep.${NAM[$i]} --recode --recode-INFO-all --minDP 5 --out ${NAM[$i]}
 
-paste <(mawk '!/#/' ${NAMES[$i]}.recode.vcf | cut -f1,2,10 | cut -f1 -d ":") <(mawk '!/#/' ${NAM[$i]}.recode.vcf | cut -f1,2,10 | cut -f1 -d ":") | mawk '$3 != $6' | mawk '!/\./' | cut -f1,2 > bad.loci.${NAMES[$i]}
+paste <(mawk '!/#/' ${NAMES[$i]}.recode.vcf | cut -f1,2,10 | cut -f1 -d ":") <(mawk '!/#/' ${NAM[$i]}.recode.vcf | cut -f1,2,10 | cut -f1 -d ":") | mawk '$3 != $6' | mawk '!/\./' | cut -f1,2 > bad.loci.${NAMES[$i]}.${NAM[$i]}
 
 done
 
-cat bad.loci.${NAMES[0]} > total.bad.loci
+
+NAMES=( `cut -f1  $2 | sort | uniq `)
+LEN=( `cut -f1 $2 | sort | uniq | wc -l `)
+LEN=$(($LEN - 1))
+
+
+cat bad.loci.${NAMES[0]}.* > total.bad.loci
 rm ${NAMES[0]}.recode.vcf ${NAM[0]}.recode.vcf keep.${NAMES[0]} keep.${NAM[0]}
 
 for ((i = 1; i <= $LEN; i++));
 do
-cat bad.loci.${NAMES[$i]} >> total.bad.loci 
+cat bad.loci.${NAMES[$i]}.* >> total.bad.loci 
 rm ${NAMES[$i]}.recode.vcf ${NAM[$i]}.recode.vcf keep.${NAMES[$i]} keep.${NAM[$i]}
 
 done
 
 cat total.bad.loci | perl -e 'while (<>) {chomp; $z{$_}++;} while(($k,$v) = each(%z)) {print "$v\t$k\n";}' > mismatched.loci
 rm total.bad.loci
-
